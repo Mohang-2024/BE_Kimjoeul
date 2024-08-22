ecb537bba71cafe0481a3f9b6c37fe8329f04498
kimjoeul <joeul13@naver.com>
2024-08-22 22:59:20 +0900

    project :: setting

---
+++ src/main/java/com/example/mohangbackend/MohangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mohangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohangBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mohangbackend/MohangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mohangbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
