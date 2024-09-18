8ef8d7a72f22cf39d2de1bad317e99a987140ac3
sorim <enhld00@gmail.com>
2024-09-18 23:28:48 +0900

    ⚡️ :: 에러 코드 추가

--- src/main/java/com/example/mohangbackend/global/exception/error/ErrorCode.java
+++ src/main/java/com/example/mohangbackend/global/exception/error/ErrorCode.java
@@ -8,8 +8,13 @@ import lombok.Getter;
 public enum ErrorCode {
 
     PASSWORD_MISMATCH(401, "Password Mismatched"),
+    EXPIRED_JWT(401, "Expired Jwt"),
+    INVALID_JWT(401, "Invalid Jwt"),
+    INVALID_REFRESH_TOKEN(401, "Invalid Refresh Token"),
     USER_NOT_FOUND(404, "User Not Found"),
     USER_ALREADY_EXIST(409, "User Already Exits"),
+
+    REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
