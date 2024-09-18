d880d7e9ae141bde67e2e96e4d8b25edd7cd7654
sorim <enhld00@gmail.com>
2024-09-18 23:26:56 +0900

    ⚡️ :: 유저 컨트롤러 추가

---
+++ src/main/java/com/example/mohangbackend/domain/auth/presentation/UserController.java
@@ -0,0 +1,39 @@
+package com.example.mohangbackend.domain.auth.presentation;
+
+import com.example.mohangbackend.domain.auth.presentation.dto.TokenResponse;
+import com.example.mohangbackend.domain.auth.service.ReissueService;
+import com.example.mohangbackend.domain.user.presentation.dto.SignInRequest;
+import com.example.mohangbackend.domain.user.presentation.dto.SignUpRequest;
+import com.example.mohangbackend.domain.user.service.UserSignInService;
+import com.example.mohangbackend.domain.user.service.UserSignUpService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class UserController {
+
+    private final UserSignInService userSignInService;
+    private final UserSignUpService userSignUpService;
+    private final ReissueService reissueService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public void signUp(@Valid @RequestBody SignUpRequest request) {
+        userSignUpService.signup(request);
+    }
+
+    @PostMapping("/signin")
+    public TokenResponse signIn(SignInRequest request) {
+        return userSignInService.signIn(request);
+    }
+
+    @PostMapping("/reissue")
+    public TokenResponse reissue(@RequestHeader(name = "AUTHORIZATION_HEADER")String refreshToken) {
+        return reissueService.userReissue(refreshToken);
+    }
+
+}
