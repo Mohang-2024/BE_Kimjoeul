e955f86d69029f0735550935ee1046e23e970c51
sorim <enhld00@gmail.com>
2024-09-18 23:28:24 +0900

    ⚡️ :: JWT 설정

---
+++ src/main/java/com/example/mohangbackend/global/properties/JwtProperty.java
@@ -0,0 +1,18 @@
+package com.example.mohangbackend.global.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperty {
+
+    private final String secretKey;
+    private final Long accessExp;
+    private final Long refreshExp;
+    private final String header;
+    private final String prefix;
+
+}
