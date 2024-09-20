33d0d53a9ee68d43b4899dfc1a859c5a00ad93fd
sorim <enhld00@gmail.com>
2024-09-20 15:42:30 +0900

    📝 :: url 수정

--- src/main/java/com/example/mohangbackend/domain/auth/presentation/UserController.java
+++ src/main/java/com/example/mohangbackend/domain/auth/presentation/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/usercontroller")
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
